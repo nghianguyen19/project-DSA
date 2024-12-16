477efe1f4d14c5b5f092daef7eb28bc18215e983
ITITIU21148 <nuptronggoc123@gmail.com>
2024-12-16 23:20:50 +0700

    Some stuff

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.awt.event.KeyListener;
+
+public class Game implements KeyListener {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+
+    private GamePanel gamePanel;
+}
